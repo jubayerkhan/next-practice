58df9f488bb6b2fba094e7b52ebd9099497c964c
@jubayer.khan <jubayer.khan@annanovas.com>
2026-02-25 16:30:51 +0600

    loader update

--- app/loading/page.js
+++ app/loading/page.js
@@ -6,7 +6,7 @@ export default function Loading() {
   const [connected, setConnected] = useState(false);
 
   return (
-    <div className="">
+    <div className="max-w-2xl mx-auto p-6">
         <span className="loader"></span>
     </div>
   );
